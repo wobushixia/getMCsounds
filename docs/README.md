68bf6fb5236d78893b935e656ed719f53a126226
5xian_39 <XiaRtdfuy@outlook.com>
2024-08-21 11:11:14 +0800

    getMCsounds-V1.0

---
+++ docs/README.md
@@ -0,0 +1,24 @@
+### getMCsounds
+## 项目语言
+项目语言由C#编写
+## 运行项目
+step1:下载源码
+
+step2:运行并成功打开命令框
+
+step3:输入你的.minecraft绝对路径(如D:\PCL\.minecraft,根据你自己的.minecraft绝对路径来填)
+
+step4:输入json版本(如1.8.json,根据你indexes路径下的json版本来填)
+
+step5:输入导出路径(请使用空文件夹aka.你不要导出过了又导出一遍,会报错,我没做try-catch)
+## 注意事项
+确保你下载了"Newtonsoft.json"库
+
+打开Powershell
+
+cd你下载好源码的目录
+<code>Install-Package Newtonsoft.json -ProjectName MTools</code>
+Tips:把MTools改成你的下下来源码之后的项目名
+
+## 关于
+项目由5xian39编写
---
+++ src/Program.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace MTools
+{
+    class GetMcSounds
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入 Minecraft 路径（路径最后不用带 \\）：");
+            string minecraftPath = Console.ReadLine();
+            Console.WriteLine("请输入json版本(如1.8.json)");
+            string jsonFilePath = Path.Combine(minecraftPath, "assets", "indexes", Console.ReadLine());
+            Console.WriteLine("正在解析 JSON 文件...");
+            var (hashes, names) = AnalyseJson(jsonFilePath);
+
+            Console.WriteLine("解析成功！");
+            Console.WriteLine("请输入导出路径：");
+            string exportPath = Console.ReadLine()+Path.DirectorySeparatorChar;
+
+            for (int i = 0; i < hashes.Length; i++)
+            {
+                string objectsPath = Path.Combine(minecraftPath, "assets", "objects");
+                string sourceFilePath = Path.Combine(objectsPath, TakeString(hashes[i]), hashes[i]);
+                Path.Combine(); 
+
+                string targetFilePath = Path.Combine(exportPath, names[i]);
+                string FolderPath = "";
+                string[] folderList = names[i].Split("/");
+                for (int j = 0; j <= folderList.Length-2; j++)
+                    FolderPath += folderList[j] + Path.DirectorySeparatorChar;
+                string targetFolderPath = exportPath + FolderPath;
+                Console.WriteLine(targetFolderPath);
+                CreateFolder(exportPath, targetFolderPath);
+                CopyFile(sourceFilePath, exportPath+names[i]);
+
+                int progress = (i + 1) * 100 / hashes.Length;
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write($"Progress: [{new string('#', progress / 2)}{new string(' ', 50 - progress / 2)}] {progress}%");
+            }
+            Console.WriteLine("\n操作完成！");
+        }
+
+        static (string[] hash, string[] name) AnalyseJson(string jsonPath)
+        {
+            string jsonContent = File.ReadAllText(jsonPath);
+            JObject data = JObject.Parse(jsonContent);
+            JObject objects = (JObject)data["objects"];
+
+            List<string> names = new List<string>();
+            List<string> hashes = new List<string>();
+
+            foreach (var item in objects)
+            {
+                string key = item.Key;
+                JObject value = (JObject)item.Value;
+                names.Add(key);
+                hashes.Add((string)value["hash"]);
+            }
+
+            return (hashes.ToArray(), names.ToArray());
+        }
+
+        static void CopyFile(string sourcePath, string targetPath)
+        {
+            if (File.Exists(sourcePath))
+            {
+                File.Copy(sourcePath, targetPath);
+            }
+        }
+
+        static string TakeString(string str)
+        {
+            return str.Length < 2 ? str : str.Substring(0, 2);
+        }
+
+        static void CreateFolder(string path, string str)
+        { 
+            path = Path.Combine(path,str);
+            Directory.CreateDirectory(path);
+        }
+    }
+}
